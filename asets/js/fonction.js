d29f8a56d8786250921a9bf1e6d619d6db391f61
DamienPoix <damienpoix@live.fr>
2018-06-25 10:41:50 +0200

    Exo5 part2

---
+++ asets/js/fonction.js
@@ -0,0 +1,15 @@
+function getImage1(){
+var change = document.getElementById ('image1') .src='asets/img/image1_2.jpg';
+}
+function getImage2(){
+var change2 = document.getElementById ('image2') .src='asets/img/image2_2.jpg';
+}
+function getImage3(){
+var change3 = document.getElementById ('image3') .src='asets/img/image3_2.jpg';
+}
+function getImage4(){
+var change4 = document.getElementById ('image4') .src='asets/img/image4_2.jpg';
+}
+function getImage5(){
+var change5 = document.getElementById ('image5') .src='asets/img/image5_2.jpg';
+}
